828e88a7e7a84366e045c23f89932247dbc535c9
Lin Clark <lin.w.clark@gmail.com>
2015-07-15 21:20:01 -0400

    use babel-loader for .js files too

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'babel-loader' },
+      { test: /\.jsx?$/, loader: 'babel-loader' },
       { test: /\.css/, loader: 'css-loader!cssnext-loader' },
       { test: /\.md$/, loader: 'markdown-with-front-matter' }
     ]

89ef05807311b7c031681d781baa182d2020ca11
Lin Clark <lin.w.clark@gmail.com>
2015-08-15 18:08:44 -0400

    Revert "attempt using fittext to scale text"
    This reverts commit 4e97c88eb01242285e4dcb70b0460b4b23da2140.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,17 @@
 // components/Header.jsx
 var React = require('react')
 var Router = require('react-router')
-var fittext = require('raw!./../node_modules/fittext/fittext.js')
 
 var Header = React.createClass({
   render: function () {
     return (
       <header>
         <div id="intro">
-          <p className='resize'>Hi, I'm <a href='/'>Lin Clark</a></p>
-          <p className='resize2'>I code and talk about code.</p>
+          <p>Hi, I'm <a href='/'>Lin Clark</a></p>
+          <p>I code and talk about code.</p>
           <p>I like to contribute. You'll find me writing and talking about things you can do with JavaScript.</p>
           <p>In a past life I worked at npm, developed core modules for Drupal, created MicrodataPHP, and contributed to W3C standardization efforts, among other OSS contributions.</p>
         </div>
-        <script dangerouslySetInnerHTML={{ __html: fittext.toString() + `var el = document.querySelector('#intro .resize'); window.fitText(el); var el2 = document.querySelector('#intro .resize2'); window.fitText(el2, 1.3)`}}></script>
       </header>
     )
   }

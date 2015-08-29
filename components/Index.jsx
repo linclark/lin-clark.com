0ff49f811e5a22966afe61ff5177dc6450dbb812
Lin Clark <lin.w.clark@gmail.com>
2015-08-28 22:58:09 -0400

    add sample speaking content

--- components/Index.jsx
+++ components/Index.jsx
@@ -5,18 +5,35 @@ var ContentStore = require('./../stores/ContentStore')
 var Index = React.createClass({
   getInitialState: function () {
     return {
-      contentList: ContentStore.getContent(5)
+      contentList: ContentStore.getContent(3)
     }
   },
   render: function () {
     return (
       <main id="home">
-        <div className="card">
-          <section>
-            <h2>Writing</h2>
-            <ContentList contentList={this.state.contentList} />
-          </section>
-        </div>
+        <section>
+          <h2>writing</h2>
+          <ContentList contentList={this.state.contentList} />
+        </section>
+        <section>
+          <h2>speaking</h2>
+            <div className="content-list">
+              <ul>
+                <li><a href="http://slides.com/linclark/webpack/">
+                  <div className="title">Making small modules actually work with Webpack and npm</div>
+                  <div className="summary">React Rally</div>
+                </a></li>
+                <li><a href="http://slides.com/linclark/modularity/">
+                  <div className="title">The Evolution of Modularity</div>
+                  <div className="summary">NodeDay London</div>
+                </a></li>
+                <li><a href="https://www.youtube.com/watch?v=j9Iak-jiFz0">
+                  <div className="title">Putting it all together</div>
+                  <div className="summary">jQuerySF</div>
+                </a></li>
+              </ul>
+            </div>
+        </section>
       </main>
     )
   }

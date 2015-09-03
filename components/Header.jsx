54d8f4a5713e45c1a7af3d4a861984afb186cf90
Lin Clark <lin.w.clark@gmail.com>
2015-09-03 17:15:30 -0400

    style inside page header
    Fixes #7

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,14 +2,22 @@
 var React = require('react')
 var Router = require('react-router')
 var IntroHeader = require('./IntroHeader')
+var githubImg = "/" + require('./../images/github.svg')
+var twitterImg = "/" + require('./../images/twitter.svg')
 
 var Header = React.createClass({
   render: function () {
-    var hi = <div className="hello">Hi, I'm <a href='/'>Lin Clark</a></div>;
+    var hi = <div><div className="headshot"></div><div className="hello">Hi, I'm <a href='/'>Lin Clark</a></div></div>;
     var h = (this.props.path == "/") ? <IntroHeader>{hi}</IntroHeader> : {hi};
     return (
       <header>
         {h}
+        <div className="find-me">
+          <a href="https://twitter.com/linclark"><img src={twitterImg} /></a>
+          <span className="divider">&#10731;</span>
+          <a href="https://github.com/linclark/"><img src={githubImg} /></a>
+        </div>
+        <hr />
       </header>
     )
   }
--- components/IntroHeader.jsx
+++ components/IntroHeader.jsx
@@ -1,24 +1,15 @@
 var React = require('react')
-var githubImg = require('./../images/github.svg')
-var twitterImg = require('./../images/twitter.svg')
 
 var Header = React.createClass({
   render: function () {
     return (
       <div id="intro">
-        <div className="headshot"></div>
         {this.props.children}
         <p className="tagline">I code and talk about code.</p>
         <div className="bio">
           <p>I like to contribute. You'll find me writing and talking about things you can do with JavaScript.</p>
           <p>In a past life I worked at npm, developed core modules for Drupal, created MicrodataPHP, and contributed to W3C standardization efforts, among other OSS contributions.</p>
         </div>
-        <div className="find-me">
-          <a href="https://twitter.com/linclark"><img src={twitterImg} /></a>
-          <span className="divider">&#10731;</span>
-          <a href="https://github.com/linclark/"><img src={githubImg} /></a>
-        </div>
-        <hr />
       </div>
     )
   }
